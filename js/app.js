791264288e2ddb0c7b472d6b1710388f221b9192
Сергій <kjserg@i.ua>
2025-10-01 17:25:43 +0300

    працюю над filters

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const doc = document;
 let duration = 400;
+let windowWidthReconfiguration = 1400;
 doc.addEventListener('DOMContentLoaded', function () {
     doc.querySelectorAll('.theme-switcher').forEach(function (element) {
         element.addEventListener('click', function (e) {
@@ -56,8 +57,7 @@ doc.addEventListener('DOMContentLoaded', function () {
     });
     doc.querySelectorAll('.custom-modals').forEach(function (element) {
         element.addEventListener('click', function (e) {
-            e.preventDefault();
-            if(!e.target.classList.contains('custom-modals')) return;
+            if (!e.target.classList.contains('custom-modals')) return;
             removeClassFromElements('.custom-modal-active', 'custom-modal-active');
             _$('body').classList.remove('open-custom-modal');
         });
@@ -66,6 +66,13 @@ doc.addEventListener('DOMContentLoaded', function () {
         element.addEventListener('click', function (e) {
             e.preventDefault();
             const sidebar = _$('.sidebar');
+            const windowOuterWidth = window.outerWidth;
+            if(windowOuterWidth <= windowWidthReconfiguration){
+                sidebar.classList.remove('sidebar--showed-mobile');
+                return;
+            }
+            if (sidebar.classList.contains('sidebar--hidden')) return;
+            sidebar.classList.add('sidebar--hidden');
             const section = sidebar.closest('.dashboard-section');
             slideLeft(sidebar, duration);
             section.classList.add('dashboard-section--full');
@@ -75,13 +82,75 @@ doc.addEventListener('DOMContentLoaded', function () {
         element.addEventListener('click', function (e) {
             e.preventDefault();
             const sidebar = _$('.sidebar');
+            const windowOuterWidth = window.outerWidth;
+            if(windowOuterWidth <= windowWidthReconfiguration){
+                sidebar.classList.add('sidebar--showed-mobile');
+                return;
+            }
+            if (!sidebar.classList.contains('sidebar--hidden')) return;
+            sidebar.classList.remove('sidebar--hidden');
             const section = sidebar.closest('.dashboard-section');
             slideRight(sidebar, duration);
             section.classList.remove('dashboard-section--full');
         });
     });
+    doc.querySelectorAll('.accordion__head').forEach(function (element) {
+        element.addEventListener('click', function (e) {
+            e.preventDefault();
+            const wrapper = element.closest('.accordion');
+            const content = wrapper.querySelector('.accordion__content');
+            if(wrapper.classList.contains('active')){
+                wrapper.classList.remove('active');
+                slideUp(content, duration)
+            }else {
+                wrapper.classList.add('active');
+                slideDown(content, duration)
+            }
+        });
+    });
 });
 
+
+function slideDown(element, duration = 400) {
+    element.style.removeProperty('display');
+    let display = window.getComputedStyle(element).display;
+
+    if (display === 'none') {
+        element.style.display = 'block';
+    }
+
+    let height = element.offsetHeight;
+    element.style.overflow = 'hidden';
+    element.style.height = 0;
+    console.log(height)
+    setTimeout(() => {
+        element.style.transition = `${duration}ms ease`;
+        element.style.height = height + 'px';
+    }, 10);
+
+    setTimeout(() => {
+        element.style.removeProperty('height');
+        element.style.removeProperty('overflow');
+        element.style.removeProperty('transition');
+    }, duration);
+}
+
+function slideUp(element, duration = 400) {
+    element.style.height = element.offsetHeight + 'px';
+    element.style.overflow = 'hidden';
+    element.style.transition = `${duration}ms ease`;
+
+    setTimeout(() => {
+        element.style.height = 0;
+    }, 10);
+
+    setTimeout(() => {
+        element.style.display = 'none';
+        element.style.removeProperty('height');
+        element.style.removeProperty('overflow');
+        element.style.removeProperty('transition');
+    }, duration);
+}
 
 function slideRight(element, duration = 400) {
     element.classList.add('animation-execution');
@@ -144,7 +213,6 @@ function addClassToElements(selector, cssClass) {
 function _$(selector) {
     return doc.querySelector(selector);
 }
-
 
 function setThemeForHtml(theme) {
     doc.documentElement.setAttribute('data-theme', theme);
